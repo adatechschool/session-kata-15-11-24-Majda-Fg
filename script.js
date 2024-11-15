2bb6962f52c847b85709a61cb741c768e85174f7
Majda-Fg <majda.fougou@gmail.com>
2024-11-15 13:07:22 +0100

    étape 2

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
+
 function getLatinCharacterList(text){
     text = "Hello, world";
     let characters = text.split('');
-    console.log(characters)
+    // console.log(characters)
 }
 getLatinCharacterList()
+
+function translateLatinCharacter(character){
+    for(let [key, value] of Object.entries(latinToMorse)){
+        // console.log(`${key}: ${value}`);
+        if(character === key){
+            return(value) 
+        }
+      }
+}
+console.log(translateLatinCharacter('B'))
